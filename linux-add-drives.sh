fba6a1d12fb9498397f1724bf19f33acc609ff4d
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2018-02-21 12:16:24 -0800

    Improved mountcode cleanup changed credential file permission from 600 to 400 Removed -l list from ls

--- linux-add-drives.sh
+++ linux-add-drives.sh
@@ -65,11 +65,9 @@ touch $CREDENTIAL_FILE
 # change owner to root
 sudo chown root:root $CREDENTIAL_FILE
 # change permissions so no one except root can view file
-sudo chmod 600 $CREDENTIAL_FILE
+sudo chmod 400 $CREDENTIAL_FILE
 # Insert credentials into file
 sudo echo -e "username=$UI_USER\npassword=$UI_PASS" >> $CREDENTIAL_FILE
-echo "To change your password, rerun this script or edit the file directly"
-sleep 1
 
 # create mount points if they dont exist
 echo -e "\nCreating mount points"
@@ -85,33 +83,34 @@ if [ ! -d "$MOUNT_DIR_S" ]; then
 else
   echo "$MOUNT_DIR_S already exists"
 fi
-sleep 1
 
 
 echo -e "\nInstalling cifs-utils"
 sudo apt-get update -q
 sudo apt-get install cifs-utils || echo -e "\nInstalling cifs-utils failed. Please try manually installing cifs-utils"; exit
 
-# this works for command line:
-#sudo mount -t cifs //files.uidaho.edu/shared /mnt/sdrive -o credentials=/home/nick/GitRepos/Uidaho-its-scripts/ui-smbcredentials -rw
-
 echo -e "\nCreating mount entry in /etc/fstab"
 MOUNTCODE_U="//$ADDR_U $MOUNT_DIR_U cifs credentials=$CREDENTIAL_FILE,uid=$LUSER,gid=$LUSER 0 0"
 MOUNTCODE_S="//$ADDR_S $MOUNT_DIR_S cifs credentials=$CREDENTIAL_FILE,uid=$LUSER,gid=$LUSER 0 0"
 echo "Mountcode U: $MOUNTCODE_U"
 echo "Mountcode S: $MOUNTCODE_S"
-#check if fstab already has this line. if not add it.
-grep -q -F "$MOUNTCODE_U" /etc/fstab || echo "$MOUNTCODE_U" >> /etc/fstab
-grep -q -F "$MOUNTCODE_S" /etc/fstab || echo "$MOUNTCODE_S" >> /etc/fstab
+
+# find the line that matches /mnt/sdrive then delete line
+sed -i '/\/mnt\/udrive/D' /etc/fstab
+sed -i '/\/mnt\/sdrive/D' /etc/fstab
+
+# Add mountcode to fstab if substring is not found
+grep -F "$MOUNT_DIR_U" /etc/fstab || echo "$MOUNTCODE_U" >> /etc/fstab
+grep -q -F "$MOUNT_DIR_S" /etc/fstab || echo "$MOUNTCODE_S" >> /etc/fstab
 
 echo -e "\nMounting the drives with the command \"sudo mount -a\""
-sudo mount -a               # mount the ramdisk
+sudo mount -a     # mount the ramdisk
 
 echo -e "\n---------------------------------------"
 echo -e "These are the files in your udrive"
-ls -l $MOUNT_DIR_U
+ls $MOUNT_DIR_U
 echo -e "\nThese are the files in your sdrive"
-ls -l $MOUNT_DIR_S
+ls $MOUNT_DIR_S
 
 # instructions
 echo "These drives will only auto connect at startup and only when connected to AirVandalGold or ethernet"
